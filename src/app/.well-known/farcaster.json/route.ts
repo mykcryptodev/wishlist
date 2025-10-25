483d1a58cd504e131a0ed672eb89dea48c8d30a0
mykcryptodev <mykcryptodev@gmail.com>
2025-10-25 12:47:55 -0400

    no well known

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,11 +4,9 @@ export async function GET() {
   const URL = process.env.NEXT_PUBLIC_URL as string;
   return Response.json({
     accountAssociation: {
-      header:
-        "eyJmaWQiOjIxNzI0OCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGViYTc4NzE3YjZmMDU5Q0ZFMGI3NUU3NUMyZWQ0QkI3Y0E2NTE1NEYifQ",
-      payload: "eyJkb21haW4iOiJmb290YmFsbC1vbmNoYWluLnZlcmNlbC5hcHAifQ",
-      signature:
-        "MHhmZDFlMzlmZTM2Yjc5YjVlMTRiNzAzMjNkOGU5YTEwN2IwMmQ3NjQ5ODU4MzI2OTEyM2FmYTY3ODk5MjkwZmY4MjY4MTBkNTViOTNlZGEzYTA2YWZhYmJjMDQ3NzVmNTA3ZDg0Mjg5N2U2Y2M2YzcyZTQxZmM3ODk5NTdiZTc3NTFi",
+      header: "",
+      payload: "",
+      signature: "",
     },
     baseBuilder: {
       allowedAddresses: ["0x515B4Ff55078066BA8B025a1e974215084FE86d5"],
